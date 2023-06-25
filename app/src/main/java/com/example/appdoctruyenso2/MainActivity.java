da9cd5afc37f614e0eeb38171e1ba3a65e5d8048
nguyentien <nguyentien@.gmail>
2023-06-26 00:09:27 +0700

    navbar va icon search

--- app/src/main/java/com/example/appdoctruyenso2/MainActivity.java
+++ app/src/main/java/com/example/appdoctruyenso2/MainActivity.java
@@ -1,11 +1,19 @@
 package com.example.appdoctruyenso2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.graphics.drawable.Animatable;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
@@ -22,6 +30,8 @@ public class MainActivity extends AppCompatActivity {
     ViewFlipper viewFlipper;
     NavigationView navigationView;
     ListView listViewTruyen,listViewMainchinh,listViewthongtin;
+    DrawerLayout drawerLayout;
+
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,9 +45,29 @@ public class MainActivity extends AppCompatActivity {
         listViewthongtin = findViewById(R.id.listThongtin);
         listViewMainchinh = findViewById(R.id.listMainchinh);
         //goij ham
+        ActionBar();
         AcctionViewFlipper();
 
+
     }
+    //thanh actionBar với toolBar
+    private void ActionBar() {
+        //hàm hỗ trợ toolbar
+        setSupportActionBar(toolbar);
+        //khởi tạo nút cho actionBar
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        //tạo icon cho toolBar
+        toolbar.setNavigationIcon(android.R.drawable.ic_menu_sort_by_size);
+        //tạo sự kiện
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+
+    }
+
     //phuong thuc chay quangr cao
     private void AcctionViewFlipper() {
         //mang chua tam anh cho quang cao
@@ -60,7 +90,7 @@ public class MainActivity extends AppCompatActivity {
             viewFlipper.addView(imageView);
         }
         //thiet lap tu dong chay
-        viewFlipper.setFlipInterval(4000);
+        viewFlipper.setFlipInterval(3000);
         //run flipper
         viewFlipper.setAutoStart(true);
 
@@ -70,5 +100,25 @@ public class MainActivity extends AppCompatActivity {
         //goi anination vao viewFlipper
         viewFlipper.setInAnimation(animation_in);
         viewFlipper.setInAnimation(animation_out);
+    }
+    //nạp 1 menu tìm kiếm vào acctionBar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.my_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        //neu click vào tim kiem sẽ chuyển trnag
+        if (item.getItemId() == R.id.menul) {
+            // Handle the menu item with the specified ID
+            Intent intent = new Intent(MainActivity.this, mainTimkiem.class);
+            startActivity(intent);
+            return true;
+        } else {
+            // Handle other menu items
+            return super.onOptionsItemSelected(item);
+        }
     }
 }
---
+++ app/src/main/java/com/example/appdoctruyenso2/mainTimkiem.java
@@ -0,0 +1,14 @@
+package com.example.appdoctruyenso2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class mainTimkiem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_timkiem);
+    }
+}
